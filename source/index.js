a4823ad4e8772b9db1391d964f1b9afc4b88c50b
yogjin <youngjin988@gmail.com>
2023-03-05 14:01:06 +0900

    feat: webpack을 이용해 js 파일들을 번들링
    npx webpack --entry ./source/index.js --output-path ./dist/bundle_index.js 로 번들링 후 html에서 가져다 사용

--- source/index.js
+++ source/index.js
@@ -1,4 +1,4 @@
-import hello_word from './source/hello.js';
-import world_word from './source/world.js';
+import hello_word from './hello.js';
+import world_word from './world.js';
 
 document.querySelector('#root').innerHTML = `${hello_word} ${world_word}`;
