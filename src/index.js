e1e14fa3fce674a0dff71fbfaef42a633b39c19c
yogjin <youngjin988@gmail.com>
2023-03-06 02:02:16 +0900

    `npm module' webpack이 알아서 node_modules의 경로를 찾아준다.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import style from './style/index.css';
 import hello_word from './hello.js';
 import world_word from './world.js';
+import _ from 'lodash';
 
-document.querySelector('#root').innerHTML = `${hello_word} ${world_word}`;
+document.querySelector('#root').innerHTML = _.join([hello_word, world_word], ',');
